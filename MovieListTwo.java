8c69d04c0a7e1d289ac5a7f7d5fe5b3e65e1ad1d
GBTC440003UR <GBTC440003UR@MC.MONTGOMERYCOLLEGE.EDU>
2018-06-04 10:12:16 -0400

    Initial commit

---
+++ MovieListTwo.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+
+public class MovieListTwo {
+
+    public static void main (String[] args){
+    Scanner in = new Scanner(System.in);
+    String [] movies = new String [100];
+    final String SENTINEL = "q";
+    String userInput = "";
+    int index = 0;
+
+    while (!userInput.equals(SENTINEL)) {
+        System.out.println("Enter the name of your favorite movie (press: q to quit) :");
+        userInput = in.nextLine();
+        if (!userInput.toLowerCase().equals(SENTINEL)) {
+            movies[index] = userInput;
+            index++;
+        }
+        else {
+            userInput = SENTINEL;
+        }
+    }
+    for (int i =0; i < index; i++){
+            System.out.println(movies[i]);
+}
+}
+}
